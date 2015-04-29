8e580595746b1c81b5da79049d1947f0edeb8244
unknown <aludena18@gmail.com>
2015-04-29 00:27:29 -0500

    v 1.1

--- PhoneLocation/src/com/abel/phonelocation/MainActivity.java
+++ PhoneLocation/src/com/abel/phonelocation/MainActivity.java
@@ -1,14 +1,16 @@
 package com.abel.phonelocation;
 
 import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	TextView tvPosicion;
+	TextView tvPosicion, tvHello;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +18,17 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		tvPosicion = (TextView)findViewById(R.id.txvPosicion);
+		tvHello = (TextView)findViewById(R.id.txvHello);
+		
+		/*CODIGO PARA ACTIVAR EL GPS Y OBTENER LOS DATOS*/
 		MiLocationListener lListener = new MiLocationListener();
 		LocationManager lm = (LocationManager)getSystemService(LOCATION_SERVICE);
 		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, lListener);
+		
+		/*CODIGO PARA OBTENER EL IMEI DEL CELU*/
+		TelephonyManager tm = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+		String imei = tm.getDeviceId();
+		tvHello.setText(imei);
 	}
 	
 	public class MiLocationListener implements LocationListener{
@@ -29,12 +39,16 @@ public class MainActivity extends Activity {
 			String latitud = Double.toString(loc.getLatitude());
 			String longitud = Double.toString(loc.getLongitude());
 			String altitud = Double.toString(loc.getAltitude());
+			String time = Long.toString(loc.getTime());
+			String vel = Float.toString(loc.getSpeed());
 			String direccion = Float.toString(loc.getBearing());
 			
 			tvPosicion.setText("La posicion actual es: \n" +
 					"Latitud = " + latitud + "\n" +
 					"Longitud = " + longitud + "\n" +
 					"Altitud = " + altitud + "\n" +
+					"Fecha y Hora = " + time + "\n" +
+					"Velocidad = " + vel + "\n" +
 					"Direccion = + " + direccion);
 		}
 
