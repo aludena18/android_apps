f1441c97e4ab63cda9c097c73d0dd1aa6e548102
Abel Ludena <aludena18@gmail.com>
2015-05-13 18:26:03 -0500

    algOffice

--- GeolocalizadorGSM/gen/com/alg/geolocalizadorgsm/R.java
+++ GeolocalizadorGSM/gen/com/alg/geolocalizadorgsm/R.java
@@ -25,7 +25,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080002;
+        public static final int txvDatosFono=0x7f080001;
         public static final int txvMensaje=0x7f080000;
     }
     public static final class layout {
@@ -37,6 +38,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int etqTxvDatosFono=0x7f050004;
         public static final int etqTxvMensaje=0x7f050003;
         public static final int hello_world=0x7f050001;
     }
