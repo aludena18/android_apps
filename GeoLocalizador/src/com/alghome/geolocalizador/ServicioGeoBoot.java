7e32b42b39b3f7ed5654dd0fb508dc8bb9b75b2f
unknown <aludena18@gmail.com>
2015-05-28 00:10:50 -0500

    algHome

--- GeoLocalizador/src/com/alghome/geolocalizador/ServicioGeoBoot.java
+++ GeoLocalizador/src/com/alghome/geolocalizador/ServicioGeoBoot.java
@@ -1,14 +1,29 @@
 package com.alghome.geolocalizador;
 
+import java.util.Timer;
+
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.IBinder;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
 import android.util.Log;
 
-import com.alghome.loclistener.MiLocManager;
+import com.alghome.datagetset.GpsGetset;
+import com.alghome.datagetset.GsmGetSet;
+import com.alghome.datasend.SendTask;
+import com.alghome.loclistener.MiLocListener;
+import com.alghome.loclistener.MiLocListenerGsm;
 
 public class ServicioGeoBoot extends Service{
-	MiLocManager mLocManager = new MiLocManager(this);
+	//MiLocManager mLocManager = new MiLocManager(this);
+	LocationManager mLocManager;
+	MiLocListener mLocListener;
+	GpsGetset datosGetSet;
+	GsmGetSet datosGsmGetSet;
+	Timer timer;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -21,14 +36,23 @@ public class ServicioGeoBoot extends Service{
 		// TODO Auto-generated method stub
 		//super.onCreate();
 		Log.d("SERVICIO GEO BOOT", "Servicio creado");
-		mLocManager.execute();
+		//mLocManager.execute();
+		locManager();
+		timer = new Timer();
+		SendTask task = new SendTask(datosGetSet, datosGsmGetSet, "107.172.12.220", 21020);
+		timer.scheduleAtFixedRate(task, 5000, 30*1000);
+		
 	}
+
+	
 
 	@Override
 	public void onDestroy() {
 		// TODO Auto-generated method stub
 		//super.onDestroy();
-		mLocManager.cancel(true);
+		//mLocManager.cancel(true);
+		mLocManager.removeUpdates(mLocListener);
+		timer.cancel();
 		Log.d("SERVICIO GEO BOOT", "Servicio destruido");
 	}
 
@@ -38,6 +62,21 @@ public class ServicioGeoBoot extends Service{
 		//return super.onStartCommand(intent, flags, startId);
 		Log.d("SERVICIO GEO BOOT", "Servicio iniciado");
 		return START_STICKY;
+	}
+	
+	private void locManager() {
+		// TODO Auto-generated method stub
+		mLocManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+		TelephonyManager mTManager = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+		GsmCellLocation gsmCellLoc = (GsmCellLocation)mTManager.getCellLocation();
+		
+		mLocListener = new MiLocListener(mTManager.getDeviceId(), this);
+		MiLocListenerGsm mLocListenerGsm = new MiLocListenerGsm( mTManager, gsmCellLoc);
+		
+		mLocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocListener);
+
+		datosGetSet = mLocListener.getDataGPS();
+		datosGsmGetSet = mLocListenerGsm.getDataGSM();
 	}
 
 }
--- serviceBoot/src/com/example/serviceboot/MainActivity.java
+++ serviceBoot/src/com/example/serviceboot/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		startService(new Intent(this, ServiceBoot.class));
+		//startService(new Intent(this, ServiceBoot.class));
 	}
 
 	@Override
