bcb675d648de04bd025ee2def02d2064d103990e
Abel Ludena <aludena18@gmail.com>
2015-10-19 18:34:21 -0500

    algOffice

--- HunterPersonalTrack/gen/com/alg/hunterpersonaltrack/R.java
+++ HunterPersonalTrack/gen/com/alg/hunterpersonaltrack/R.java
@@ -1091,11 +1091,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int btnPanico=0x7f05003e;
+        public static final int btnPanico=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1136,11 +1136,12 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
-        public static final int swtOnOff=0x7f05003d;
+        public static final int swtOnOff=0x7f05003e;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int txvMensaje=0x7f05003c;
+        public static final int txvImei=0x7f05003c;
+        public static final int txvMensaje=0x7f05003d;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1246,6 +1247,7 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int etqBtnPanico=0x7f0a0011;
         public static final int etqSwtOnOff=0x7f0a0010;
+        public static final int etqTxvImei=0x7f0a0013;
         public static final int etqTxvMensaje=0x7f0a0012;
         public static final int hello_world=0x7f0a000e;
     }
